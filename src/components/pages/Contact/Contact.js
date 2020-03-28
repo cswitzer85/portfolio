6448a69e2688723e9ce9217e0801a0a4de4608c5
Cody Switzer <cswitzer85@gmail.com>
2020-03-28 01:17:56 -0500

    updated css

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,15 +1,36 @@
 import React from "react";
 import "./Contact.css";
 
-
 const Contact = () => (
   <div>
-    <h1>Contact</h1>
-      <p>Cody Switzer</p>
-      <p>Based out of: Dallas, Texas</p>
-      <p><a href="https://github.com/cswitzer85">GitHub</a></p>
-      <p><a href="https://www.linkedin.com/in/cody-switzer-1429593a/">LinkedIn</a></p>
-      <p><a href="https://docs.google.com/document/d/1qAGARoCOjh_8a_d3teaMyYCx6JeLeVV_4T7eKwMAqFI/edit?usp=sharing">Resume</a></p>
+    <div className="col-lg-8 col-md-12 col-sm-12">
+      <div className="mainContainer">
+        {" "}
+        <h1>Contact</h1>
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title" id="myName">
+              Cody Switzer
+            </h4>
+            <h6 className="card-subtitle mb-2 text-muted">
+              e-mail is set to private
+            </h6>
+            <h5 className="card-subtitle">Based out of Dallas, Texas</h5>
+            <p className="card-text">
+              Thank you for viewing my portfolio! Please feel free to reach out
+              to me via the following links.
+            </p>
+            <a href="https://github.com/cswitzer85">GitHub</a>
+            <a href="https://www.linkedin.com/in/cody-switzer-1429593a/">
+              LinkedIn
+            </a>
+            <a href="https://docs.google.com/document/d/1qAGARoCOjh_8a_d3teaMyYCx6JeLeVV_4T7eKwMAqFI/edit?usp=sharing">
+              Resume
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -3,8 +3,21 @@ import "./Resume.css";
 
 const Resume = () => (
   <div>
-    <h1>Resume</h1>
-    <p><a href="https://docs.google.com/document/d/1qAGARoCOjh_8a_d3teaMyYCx6JeLeVV_4T7eKwMAqFI/edit?usp=sharing"><img src="https://raw.githubusercontent.com/cswitzer85/portfolio/master/assets/resumeScreenShot.png" id="resumePng" alt="Image of resume" /></a></p>
+    <div className="col-lg-8 col-md-12 col-sm-12">
+      <div className="mainContainer">
+        {" "}
+        <h1>Resume</h1>
+        <p>
+          <a href="https://docs.google.com/document/d/1qAGARoCOjh_8a_d3teaMyYCx6JeLeVV_4T7eKwMAqFI/edit?usp=sharing">
+            <img
+              src="https://raw.githubusercontent.com/cswitzer85/portfolio/master/assets/resumeScreenShot.png"
+              id="resumePng"
+              alt="Image of resume"
+            />
+          </a>
+        </p>
+      </div>
+    </div>
   </div>
 );
 
