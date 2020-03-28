7cdaf6ff4cb65568cff671d2a7ee71f17dc3e630
Cody Switzer <cswitzer85@gmail.com>
2020-03-28 00:04:08 -0500

    updated resume page and contact page

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -5,11 +5,11 @@ import "./Contact.css";
 const Contact = () => (
   <div>
     <h1>Contact</h1>
-    <p>
-    Cody Switzer
-(214)717-2823
-cswitzer85@gmail.com
-    </p>
+      <p>Cody Switzer</p>
+      <p>Based out of: Dallas, Texas</p>
+      <p><a href="https://github.com/cswitzer85">GitHub</a></p>
+      <p><a href="https://www.linkedin.com/in/cody-switzer-1429593a/">LinkedIn</a></p>
+      <p><a href="https://docs.google.com/document/d/1qAGARoCOjh_8a_d3teaMyYCx6JeLeVV_4T7eKwMAqFI/edit?usp=sharing">Resume</a></p>
   </div>
 );
 
--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -4,9 +4,7 @@ import "./Resume.css";
 const Resume = () => (
   <div>
     <h1>Resume</h1>
-    <p>
-      This is where my resume goes.
-    </p>
+    <p><a href="https://docs.google.com/document/d/1qAGARoCOjh_8a_d3teaMyYCx6JeLeVV_4T7eKwMAqFI/edit?usp=sharing"><img src="https://raw.githubusercontent.com/cswitzer85/portfolio/master/Assets/resumeScreenShot.png" id="resumePng" alt="Image of resume" /></a></p>
   </div>
 );
 
